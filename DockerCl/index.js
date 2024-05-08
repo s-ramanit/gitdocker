0a349f05e92c037bd65f10f4230497bef73e97c4
manit-local <mnit72@gmail.com>
2024-05-08 19:32:23 +0700

    Add login API

--- DockerCl/index.js
+++ DockerCl/index.js
@@ -5,6 +5,11 @@ app.get('/user', (req, res) => {
     res.send({ "id": 11, "name": "Manit" });
 });
 
+app.get('/login', (req, res) => {
+    res.send("REPLY FROM LOGIN API");
+});
+
 app.listen(5000, () => {
     console.log('Express Server is Running at Port No 5000..');
 });
+
